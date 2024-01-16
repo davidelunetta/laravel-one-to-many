009990cb2bb983734fd3b8e333877c3cd6ce4fc5
davidelunetta <davidelunetta.27@gmail.com>
2024-01-17 00:56:45 +0100

    added methods to type and project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ['name', 'description', 'start_date', 'image_path'];
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Types extends Model
+class Type extends Model
 {
-    use HasFactory;
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
