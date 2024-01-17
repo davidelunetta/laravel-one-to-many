cfbf50e3dfc641a63220b715debbb68606741394
davidelunetta <davidelunetta.27@gmail.com>
2024-01-17 09:18:10 +0100

    corrected migration

--- database/migrations/2024_01_17_080919_add_type_id.php
+++ database/migrations/2024_01_17_080919_add_type_id.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('type_id', 200);
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('project');
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('type_id');
+        });
     }
 };
